648e7882dd0c95ece3e9f401bf1349e2dcd26ef1
thainguyen15 <thainguyendev@gmail.com>
2025-08-01 10:38:30 +0700

    feat: remove connect db when start app

--- backend/app.js
+++ backend/app.js
@@ -1,24 +1,12 @@
 const express = require('express');
-const mysql = require('mysql2');
 const bodyParser = require('body-parser');
 const routes = require('./routes');
 const cors = require('cors');
-const db = require('./configs/db'); // Import the db connection
-const logger = require('./utils/logger'); // Import logger
 
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
-
-db.connect((err) => {
-   if (err) {
-      logger.error(`Error connecting to MySQL: ${err.stack}`);
-      return;
-   }
-
-   logger.info('Connected to MySQL Database');
-});
 
 /* Add your routes here */
 //Health Checking
